3cc6573230eb189f384ff81ddec6707324b2a373
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-28 13:57:00 -0700

    Create a one-to-many relationship to connect answers to specific questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,10 +14,6 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     },
-    saveAnswer(params) {
-      var newAnswer = this.store.createRecord('answer', params);
-      newAnswer.save();
-      this.transitionTo('index');
-    }
+    
   }
 });
