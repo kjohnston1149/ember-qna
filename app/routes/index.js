ddedbb568c16b959e13b206987f94589440fc135
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-28 12:07:42 -0700

    Display questions and answers on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
 
   actions: {
@@ -12,6 +15,6 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
-    
+
   }
 });
