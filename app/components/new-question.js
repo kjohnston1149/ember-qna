65f5305500de22fc4f4573935ff56330f9cca583
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-28 10:42:21 -0700

    Add a save question feature/component

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
