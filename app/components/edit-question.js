4a487f9d700f36ed28b498dbb7dfcf859b1693c6
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-28 11:08:51 -0700

    Add edit feature to edit existing questions

---
+++ app/components/edit-question.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  editQuestionForm: false,
+  actions: {
+    editQuestionForm() {
+      this.set('editQuestionForm', true);
+    },
+    edit(question) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+      };
+      this.set('editQuestionForm', false);
+      this.sendAction('edit', question, params);
+    }
+  }
+});
