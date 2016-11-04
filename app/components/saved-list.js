9b2b4b3a085148f04f40681e1a975e90b85811cd
Kevin Johnston <kjohnston1149@gmail.com>
2016-11-04 09:28:19 -0700

    Add save-question service to save questions for later viewing

--- app/components/saved-list.js
+++ app/components/saved-list.js
@@ -4,9 +4,8 @@ export default Ember.Component.extend({
   savedQuestion: Ember.inject.service(),
 
   actions: {
-    saveThisQuestion(question) {
-      this.get('savedQuestion').add(question);
+    clearQuestions(question) {
+      questions: [];
     }
   }
-
 });
